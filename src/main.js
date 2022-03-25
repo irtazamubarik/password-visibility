3f7992dcc750b2740aeb63c09d7c00a1bda58344
Irtaza Mubarik <irtazamubarik@gmail.com>
2022-03-25 19:19:49 +0500

    cleaned

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faEye, faEyeSlash);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
@@ -14,6 +9,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount("#app");
